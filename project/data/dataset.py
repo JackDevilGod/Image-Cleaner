5f4acd6c2c4f80e9d1340d8722187ecf5dcda357
JackDevilGod <jack.jh666@gmail.com>
2025-07-16 23:43:26 +0200

    made dataset only need root directory

--- project/data/dataset.py
+++ project/data/dataset.py
@@ -4,11 +4,42 @@ from torch.utils.data import Dataset
 
 
 class PicturePathsDataset(Dataset):
-    def __init__(self, paths: tuple[Path]) -> None:
-        self.paths: tuple[Path] = paths
+    def __init__(self, path: Path) -> None:
+        self.paths: tuple[Path] = self._get_image_paths(path)
+
+    def _get_image_paths(self, root: Path) -> tuple[Path, ...]:
+        image_paths: list[Path] = list()
+
+        queue: list[Path] = [root]
+
+        while queue:
+            current: Path = queue.pop(0)
+
+            if current.is_dir():
+                queue += list(current.iterdir())
+            elif current.suffix.endswith((".png", ".jpg")):
+                image_paths.append(current)
+
+        return tuple(image_paths)
 
     def __len__(self) -> int:
         return len(self.paths)
 
     def __getitem__(self, index: int) -> Image.Image:
         return Image.open(self.paths[index])
+
+
+def main():
+    test = PicturePathsDataset(Path())
+
+    print(len(test))
+
+    import matplotlib.pyplot as plt
+
+    for u in range(len(test)):
+        plt.imshow(test[u])
+        plt.show()
+
+
+if __name__ == '__main__':
+    main()
